68e6936115011af566056e39c3d8b3e131caf2aa
zeroOne0012 <012012012il@gmail.com>
2025-03-26 16:53:12 +0900

    swagger test

---
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.example.demo.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("springdoc-public")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Book Everywhere API")
+                        .version("v1")
+                        .description("읽는곳 API 명세서"));
+    }
+}
+
+@RestController
+class TestMan{
+    @GetMapping("path")
+    public String getMethodName(@RequestParam String param) {
+        return new String("hi");
+    }
+    
+}
